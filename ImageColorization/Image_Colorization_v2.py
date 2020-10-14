ea1051d77fc208233d29e4d274e8b06c661676e5
FrostDayee <dhruvgarg01@gmail.com>
2020-10-14 12:37:50 +0530

    all resolutions work

--- ImageColorization/Image_Colorization_v2.py
+++ ImageColorization/Image_Colorization_v2.py
@@ -18,27 +18,34 @@ img_path = 'images/1.jpg'
 
 reconstructed_model = tf.keras.models.load_model("trained_models_v2/U-Net-epoch-100-loss-0.006095.hdf5")
 
+img = image.img_to_array(image.load_img(img_path))
+h, w = img.shape[0], img.shape[1]
+print(h, w)
+plt.imshow(rgb2lab(img/255.0)[:, :, 0])
+plt.show()
+
 img_color = []
-img = image.img_to_array(image.load_img(img_path, target_size=(256,256)))
-img_color.append(img)
+img_resize = image.img_to_array(
+    image.load_img(img_path, target_size=(256, 256, 3)))
+img_color.append(img_resize)
 img_color = np.array(img_color, dtype=float)
 # print(img_color.shape)
-img_color = rgb2lab(img_color/255.0)[:,:,:,0]
+img_color = rgb2lab(img_color/255.0)[:, :, :, 0]
 # print(img_color.shape)
 img_color = img_color.reshape(img_color.shape+(1,))
 # print(img_color.shape)
-
-plt.imshow(rgb2lab(img/255.0)[:,:,0])
-plt.show()
 
 output = reconstructed_model.predict(img_color)
 # print(output.shape)
 output = output*128
 
 result = np.zeros((256, 256, 3))
-result[:,:,0] = img_color[0][:,:,0]
-result[:,:,1:] = output[0]
+result[:, :, 0] = img_color[0][:, :, 0]
+result[:, :, 1:] = output[0]
 # print(result.shape)
 
-plt.imshow(lab2rgb(result))
+final_img = lab2rgb(result)
+final_img = cv2.resize(final_img, (w, h))
+
+plt.imshow(final_img)
 plt.show()
